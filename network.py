908c82eb77ca877f6c640de4a8ad13870f4f0ebd
IlBrugna <davidbrugnara15@gmail.com>
2025-03-07 17:54:25 +0100

    added automatic controller bootup

--- network.py
+++ network.py
@@ -4,13 +4,23 @@ from mininet.node import RemoteController
 from mininet.cli import CLI
 import random
 from topologies.topology_generator import RandomTopo
-
+import subprocess
 
 class NetworkManager:
     def __init__(self):
         self.topo = None
         self.net = None
         self.controller = RemoteController('c1', ip='127.0.0.1', port=6633)
+
+    def start_controller(self):
+        print("Starting Ryu controller...")
+        self.controller_process = subprocess.Popen(["ryu-manager", "simple_switch_stp_13.py"],stdout=open("ryu.log", "w"),stderr=subprocess.STDOUT)
+
+    def stop_controller(self):
+        if self.controller_process:
+            print("Stopping Ryu controller...")
+            self.controller_process.terminate()
+            self.controller_process.wait()
 
     def create_network(self, num_switches=5, num_hosts=10, links_prob=0.4):
         self.topo = RandomTopo(num_switches, num_hosts, links_prob)
@@ -19,6 +29,7 @@ class NetworkManager:
         self.net.build()
 
     def start_network(self):
+        self.start_controller()
         print("Starting Network...")
         self.net.start()
 
@@ -28,12 +39,13 @@ class NetworkManager:
     def stop_network(self):
         print("Shutting down...")
         self.net.stop()
-
+        self.stop_controller()
+        subprocess.run(["mn","-c"])
 
 # TEST NETWORK
 if __name__ == '__main__':
     handler = NetworkManager()
-    handler.create_network(4, 8, 0.5)
+    handler.create_network(4, 8, 0.5) #CHANGE PARAMETERS HERE!
     handler.start_network()
     
     while True:
