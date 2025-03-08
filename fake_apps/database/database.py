a239b8819ec5f88f983f837607fd029ac5175090
wannalina <annalina_wheeler@hotmail.com>
2025-03-08 11:27:53 +0100

    fix?

--- fake_apps/database/database.py
+++ fake_apps/database/database.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 # params for database connection
+GENERIC_DB_NAME = "postgres"
 DB_NAME = "cities"
 DB_USER = "postgres"
 DB_PASSWORD = "newpassword"
@@ -9,58 +10,64 @@ HOST_IP = "localhost"
 
 TABLE_VALUES = [('Trento', 'Italy'), ('Helsinki', 'Finland'), ('Riga', 'Latvia'), ('Milan', 'Italy'), ('Kuopio', 'Finland')]
 
-def establish_connection_mock():
-    connection = psycopg2.connect(
-            dbname = DB_NAME,
+# function to establish db connection
+def establish_connection(db_name):
+    try:
+        connection = psycopg2.connect(
+            dbname = db_name,
             user = DB_USER,
             password = DB_PASSWORD,
             host = HOST_IP,
             port = PORT
         )
-    connection.autocommit = True
-    cursor = connection.cursor()
-    return connection, cursor
+        connection.autocommit = True
+        cursor = connection.cursor()
+        return connection, cursor
+    except Exception as e: 
+        print(f"An error occurred: {e}")
+
+# function to close db connection
+def close_connection(connection, cursor):
+    connection.close()
+    cursor.close()
 
 # function to establish db connection
-def establish_connection():
+def connect_db():
     try:
-        connection_generic = psycopg2.connect(
-            dbname = 'postgres',
-            user = DB_USER,
-            password = DB_PASSWORD,
-            host = HOST_IP,
-            port = PORT
-        )
-        connection_generic.autocommit = True
-        cursor_generic = connection_generic.cursor()
+        connection = ''
+        cursor = ''
+
+        # establish generic db connection
+        connection_first, cursor_first = establish_connection(GENERIC_DB_NAME)
     
-        # check if the database already exists
-        cursor_generic.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
-        exists = cursor_generic.fetchone()
+        # check if mock db exists
+        cursor_first.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
+        exists = cursor_first.fetchone()
 
         if not exists:
-            connection, cursor = create_db(connection_generic, cursor_generic)
-            cursor_generic.close() 
-            connection_generic.close()
-            return connection, cursor
+            connection, cursor = create_db(connection_first, cursor_first)
         else:
-            connection, cursor = establish_connection_mock()
-            return connection, cursor
+            connection, cursor = establish_connection(DB_NAME)
+        close_connection(connection_first, cursor_first)
+        return connection, cursor
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# function to create postgresql database and table
+# function to create postgres database and table
 def create_db(connection_generic, cursor_generic):
     try:
         print("Database container is being created.")
-
+        
+        # create mock db
         cursor_generic.execute(f"CREATE DATABASE {DB_NAME};")
         connection_generic.commit()
-        cursor_generic.close() 
-        connection_generic.close()
 
-        connection, cursor = establish_connection_mock()
+        # close generic connection
+        close_connection(GENERIC_DB_NAME)
+
+        # establish connection to new db
+        connection, cursor = establish_connection(DB_NAME)
         
         # create table in db
         cursor.execute("""
@@ -82,25 +89,21 @@ def create_db(connection_generic, cursor_generic):
 # function to add mock data into database
 def add_mock_data(connection, cursor):
     try:
-        print("working 1")
-        # insert values in db table
+        # insert values in table
         cursor.executemany("INSERT INTO mock_cities_data (city, country) VALUES (%s, %s);", TABLE_VALUES)
         connection.commit()
-        
-        print("working 2")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# function to verify correctness of table
+# function to fetch db table rows
 def print_mock_table(connection, cursor):
     try:
         query = "SELECT * FROM mock_cities_data;"
-        
         cursor.execute(query)
-        rows = cursor.fetchall()
-        print("rows:", rows)
+        rows = cursor.fetchall()     
         
+        # print each table row
         for row in rows:
             print(row)
 
@@ -111,12 +114,10 @@ def print_mock_table(connection, cursor):
 # main function
 if __name__ == "__main__":
 
-    # establish connection
-    connection, cursor = establish_connection()
+    # establish db connection
+    connection, cursor = connect_db()
 
- #   add_mock_data(connection, cursor)
+    # verify db table correctness
     print_mock_table(connection, cursor)
 
-    # close connection
-    cursor.close() 
-    connection.close()
+    close_connection(connection, cursor)
