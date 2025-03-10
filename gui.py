239d939f637b7f266997ed38d9bcd5705be04d41
IlBrugna <davidbrugnara15@gmail.com>
2025-03-10 10:29:41 +0100

    added box for docker area in gui, no actions atm, only visuals

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QSpinBox, QGridLayout, QLabel, QPushButton, QHBoxLayout, QDoubleSpinBox
+from PyQt5.QtWidgets import QApplication, QWidget, QSpinBox, QGridLayout, QLabel, QPushButton, QHBoxLayout, QDoubleSpinBox,QGroupBox,QVBoxLayout,QComboBox,QScrollArea
 from PyQt5.QtCore import Qt, QObject
 from network import NetworkManager
 
@@ -16,12 +16,15 @@ class MainWindow(QWidget):
         
     def initUI(self):
         self.setWindowTitle("CONTAINER DEPLOYMENT")
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(100, 100, 600, 650)
         
-        gridLayout = QGridLayout()
-        gridLayout.setContentsMargins(30, 30, 30, 30)  # Top, Left, Bottom, Right margins
-        gridLayout.setVerticalSpacing(2)
-        gridLayout.setHorizontalSpacing(2)
+        mainLayout=QVBoxLayout();
+        #TOPOLOGY AREA
+        topoGroupBox=QGroupBox("TOPOLOGY")
+        topoLayout = QGridLayout()
+        topoLayout.setContentsMargins(30, 30, 30, 30)  # Top, Left, Bottom, Right margins
+        topoLayout.setVerticalSpacing(22)
+        topoLayout.setHorizontalSpacing(2)
         
         # Switches box
         switchesLayout = QHBoxLayout()
@@ -50,24 +53,68 @@ class MainWindow(QWidget):
         linkProbLayout.setContentsMargins(5, 0, 5, 0)
         
         #HBoxLayouts on grid
-        gridLayout.addLayout(switchesLayout, 0, 0)
-        gridLayout.addLayout(hostsLayout, 0, 1)
-        gridLayout.addLayout(linkProbLayout, 0, 2)
+        topoLayout.addLayout(switchesLayout, 0, 0)
+        topoLayout.addLayout(hostsLayout, 0, 1)
+        topoLayout.addLayout(linkProbLayout, 0, 2)
         
         self.generate = QPushButton("Generate Topology")
-        gridLayout.addWidget(self.generate, 1, 0, 1, 3, Qt.AlignHCenter)
+        topoLayout.addWidget(self.generate, 1, 0, 1, 3, Qt.AlignHCenter)
         
         self.run = QPushButton("RUN")
         self.stop = QPushButton("STOP")
-        gridLayout.addWidget(self.run, 2, 0, 1, 1, Qt.AlignRight)
-        gridLayout.addWidget(self.stop, 2, 2, 1, 1, Qt.AlignLeft)
+        topoLayout.addWidget(self.run, 2, 0, 1, 1, Qt.AlignRight)
+        topoLayout.addWidget(self.stop, 2, 2, 1, 1, Qt.AlignLeft)
+        topoGroupBox.setLayout(topoLayout)
+        mainLayout.addWidget(topoGroupBox)
+        ##TOPOLOGY FINISHED
+
+        #NET PARAMETERS SECTION TO BE ADDED HERE MAYBE???
+        # FOR EXAMPLE MAX 2H DOCKERS PER HOST
+
+        #DOCKER AREA
+        self.containerGroupBox = QGroupBox("DOCKERS")
+        containerLayout = QGridLayout()
+        
+        dockerHostsLayout=QHBoxLayout()
+        dockerHostsLayout.addWidget(QLabel("Hosts: "))
+        self.hostDropdown = QComboBox()
+        dockerHostsLayout.addWidget(self.hostDropdown)
+
+        dockerContainersLayout=QHBoxLayout()
+        dockerContainersLayout.addWidget(QLabel("Containers: "))
+        self.containerDropdown = QComboBox()
+        self.containerDropdown.addItems(["need to make", "container dictonary", "and connnect it somehow"])
+        dockerContainersLayout.addWidget(self.containerDropdown)
+
+        self.launchButton = QPushButton("Start container")
+        
+        containerLayout.addLayout(dockerHostsLayout,0,0)
+        containerLayout.addLayout(dockerContainersLayout,0,1)
+        containerLayout.addWidget(self.launchButton,0,2)
+        
+        activeContainersBox=QGroupBox("Active Containers")
+        self.activeContainerLayout = QVBoxLayout()
+        activeContainersBox.setLayout(self.activeContainerLayout)
+
+        scrollArea=QScrollArea()
+        scrollArea.setWidgetResizable(True)
+        scrollArea.setWidget(activeContainersBox)
+        containerLayout.addWidget(scrollArea,1,0,1,3)
+        
+        self.stopAllButton = QPushButton("Ferma Tutti i Container")
+        containerLayout.addWidget(self.stopAllButton, 2,1,1,1, Qt.AlignHCenter)
+    
+        self.containerGroupBox.setLayout(containerLayout)
+        mainLayout.addWidget(self.containerGroupBox)
         
         #self.open_cli = QPushButton("Open Mininet CLI")
-        #gridLayout.addWidget(self.open_cli, 3, 0, 1, 3, Qt.AlignHCenter)
+        #topoLayout.addWidget(self.open_cli, 3, 0, 1, 3, Qt.AlignHCenter)
         
         self.run.setFixedSize(100, 35)
         self.stop.setFixedSize(100, 35)
-        self.generate.setFixedSize(150, 50)
+        self.generate.setFixedSize(130, 45)
+        self.launchButton.setFixedSize(130, 30)
+        self.stopAllButton.setFixedSize(200, 45)
         
         self.run.setEnabled(False)
         self.stop.setEnabled(False)
@@ -78,7 +125,7 @@ class MainWindow(QWidget):
         self.generate.clicked.connect(self.generate_clicked)
         #self.open_cli.clicked.connect(self.open_cli_clicked)
 
-        self.setLayout(gridLayout)
+        self.setLayout(mainLayout)
     
     def run_clicked(self):
         global network_running
