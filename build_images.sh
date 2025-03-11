341cd1ff8931084d66e8e05e3dce844b51ec625c
wannalina <annalina_wheeler@hotmail.com>
2025-03-11 09:18:10 +0100

    new

--- build_images.sh
+++ build_images.sh
@@ -12,6 +12,8 @@ find fake_apps/ -mindepth 1 -maxdepth 1 -type d | while read folder_name; do
         # extract folder name from the file path
         image_name=$(basename "$folder_name")
 
+        echo "Folder name: "$folder_name", Image name: "$image_name""
+
         echo "BUILDING DOCKER IMAGE."
         docker build -t "$image_name" ./"$folder_name"
 
@@ -23,6 +25,11 @@ find fake_apps/ -mindepth 1 -maxdepth 1 -type d | while read folder_name; do
     else
         echo "No Dockerfile found in "$folder_name"."
     fi
+    echo "Cleaning up dangling images..."
+
+    echo "Removing dangling images."
+    docker image prune -f
+
 done
 
 docker images
