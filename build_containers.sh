b917b92df7429c67ba0ae0cf5fa304cc9d79ea26
wannalina <annalina_wheeler@hotmail.com>
2025-03-11 08:22:52 +0100

    fix

--- build_containers.sh
+++ build_containers.sh
@@ -7,19 +7,22 @@ echo "NUMBER OF IMAGES TO BUILD: $folder_count"
 # get and loop through all folder names in fake_apps dir on the first hierarchical level
 find fake_apps/ -mindepth 1 -maxdepth 1 -type d | while read folder_name; do
 
-    echo "FOLDER NAME: $folder_name"
-    
+    # extract folder name from the file path
+    image_name=$(basename "$folder_name")
+
+    echo "FOLDER NAME: $image_name"
+
     echo "BUILDING DOCKER IMAGE."
-    docker build -t "$folder_name" ./"$folder_name"
+    docker build -t "$image_name" ./"$image_name"
 
     echo "SAVING IMAGE TO TAR FILE."
-    docker save "$folder_name" -o "$folder_name.tar"
+    docker save "$image_name" -o "$image_name.tar"
 
     echo "LOADING NEW DOCKER IMAGE."
-    docker load -i "$folder_name.tar"
+    docker load -i "$image_name.tar"
 
     echo "RUNNING NEW DOCKER IMAGE."
-    docker run --name "$folder_name" --net=host "$folder_name"
+    docker run --name "$image_name" --net=host "$image_name"
 done
 
 docker ps -a
