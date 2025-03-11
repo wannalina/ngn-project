7b8a87bb8072ac37da140fe56b4f0c4497e6cfe1
wannalina <annalina_wheeler@hotmail.com>
2025-03-11 09:54:00 +0100

    fix

--- build_images.sh
+++ build_images.sh
@@ -7,6 +7,9 @@ echo "NUMBER OF IMAGES TO BUILD: $folder_count"
 # get and loop through all folder names in fake_apps dir on the first hierarchical level
 find fake_apps/ -mindepth 1 -maxdepth 1 -type d | while read folder_name; do
 
+    # extract folder name from the file path
+    image_name=$(basename "$folder_name")
+
     # check if Docker image exists in registry
     if docker images -q "$image_name" >/dev/null 2>&1; then
         echo "REMOVING EXISTING DOCKER IMAGE: $image_name"
