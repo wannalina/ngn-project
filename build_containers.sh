f86ac7a5e7980169e31ff5dc56cdb1b598f89061
wannalina <annalina_wheeler@hotmail.com>
2025-03-11 08:06:35 +0100

    fix

--- build_containers.sh
+++ build_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get and loop through all folder names in fake_apps dir on the first hierarchical level
-find fake_apps/ -maxdepth 1 -type d | while read folder_name; do
+find fake_apps/ -mindepth 2 -maxdepth 2 -type f | while read file_name; do
     echo "Building Docker images."
     docker build -t $folder_name .
 
@@ -9,6 +9,9 @@ find fake_apps/ -maxdepth 1 -type d | while read folder_name; do
     docker save $folder_name -o $folder_name
 
     echo "Loading new Docker images."
+    docker load -i $folder_name
+
+    echo "Running new Docker images."
     docker load -i $folder_name
 done
 
