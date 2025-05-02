5af9a870ed8ffff92511f9e06cd7c2a5d3e4c391
wannalina <annalina_wheeler@hotmail.com>
2025-05-02 14:27:07 +0200

    fix

--- topology_generator.py
+++ topology_generator.py
@@ -75,13 +75,13 @@ def handle_client(conn, net):
                     port = link.intf2 if link.intf1 == intf else link.intf1
                     switch = port.node
                     dpid = switch.dpid
-                    port_number = switch.ports[port].port_no
+                   # port_number = switch.ports[port].port_no
 
                     response = {
                         "host": host.name,
                         "switch": switch.name,
                         "dpid": dpid,
-                        "port": port_number
+                       # "port": port_number
                     }
                     conn.send(json.dumps(response).encode())
                 except Exception as e:
