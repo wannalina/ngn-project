a21ff2d9298b9f874a18500eafdea6331b4d494b
wannalina <annalina_wheeler@hotmail.com>
2025-05-23 09:06:19 +0200

    fix

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import sys
 import os
@@ -529,4 +530,4 @@ async def main():
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
