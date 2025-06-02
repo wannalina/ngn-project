97d2842dd8b62ffdb25d8e4e87189869fee8350e
wannalina <annalina_wheeler@hotmail.com>
2025-06-02 11:25:33 +0200

    fix

--- controller/simple_switch_stp_13.py
+++ controller/simple_switch_stp_13.py
@@ -109,7 +109,7 @@ class SDNController(simple_switch_13.SimpleSwitch13):
 
         # only allow packet if it's in the communication requirements
         for entry in self.communication_reqs:
-            if entry.get("host") == src_host_name and dst_host_name in entry["dependencies"]:
+            if entry["host"] == src_host_name and dst_host_name in entry["dependencies"]:
                 is_allowed = True
                 break
 
