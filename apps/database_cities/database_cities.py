bd7acead96249cbee260487afa023756d3faea64
wannalina <annalina_wheeler@hotmail.com>
2025-06-09 23:49:01 +0200

    remove unnecessary endpoint

--- apps/database_cities/database_cities.py
+++ apps/database_cities/database_cities.py
@@ -23,22 +23,6 @@ def get_cities_route():
     except Exception as e:
         return jsonify({'message': f'Error fetching cities from database: {e}', 'status_code': 500})
 
-# route to add a new city to the list
-@app.route('/add-city', methods=['POST'])
-def add_city_route():
-    try:
-        # get query params
-        city_param = request.args.get('city')
-        country_param = request.args.get('country')
-
-        # add new city to list
-        new_entry = (city_param, country_param)
-        CITIES_LIST.append(new_entry)
-
-        return jsonify({'message': 'New city added successfully', 'status_code': 200})
-    except Exception as e:
-        return jsonify({'message': f'Error adding new city to list: {e}', 'status_code': 500})
-
 # run flask app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=6000)
--- apps/server_cities/server_cities.py
+++ apps/server_cities/server_cities.py
@@ -18,24 +18,6 @@ def get_cities_route():
     except Exception as e:
         return jsonify({'message': f'Error in processing cities: {e}', 'status_code': 500})
 
-# route to add city to database
-@app.route('/add-city', methods=['POST'])
-def add_city_route():
-    try:
-        # get request params
-        # request format 'http:localhost:5000/host=4&add-city?city=Stockholm&country=Sweden
-        host = request.args.get('host')
-        city_param = request.args.get('city')
-        country_param = request.args.get('country')
-
-        # add new city
-        #new_city = {'city': city_param, 'country': country_param}
-        requests.post(f'http://10.0.0.{host}:6000/add-city?city={city_param}&country={country_param}')
-
-        return jsonify({'message': 'City added to database successfully'})
-    except Exception as e:
-        return jsonify({'message': f"Error adding city to database: {e}"})
-
 # run flask app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
