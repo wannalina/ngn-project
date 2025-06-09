038133b7c24b0d9093f4d4b354dfe505c46b3588
wannalina <annalina_wheeler@hotmail.com>
2025-06-09 11:12:44 +0200

    change port for testing

--- apps/server_cities/server_cities.py
+++ apps/server_cities/server_cities.py
@@ -24,5 +24,5 @@ def get_and_add_cities():
 
 # run flask app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
     get_and_add_cities()
